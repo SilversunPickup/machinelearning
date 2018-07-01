b49a30e26c4e5b395a4b68b337efe6b2ee1a00ed
Maximilian <mxbuchner@yahoo.de>
2018-07-01 22:37:53 +0200

    test

---
+++ bark.py
@@ -0,0 +1,40 @@
+class Dog:
+
+
+    def  __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+    def bark(self):
+        print("bark bark!")
+
+
+    def doginfo(self):
+        print(self.name + " is " + str(self.age) + " year(s) old.")
+
+    def birthday(self):
+        self.age += 1
+
+    def setBuddy(self, buddy):
+        self.buddy = buddy
+        buddy.buddy = self
+
+
+ozzy = Dog("Ozzy", 2)
+skippy = Dog("Skippy", 2)
+filou = Dog("Filou", 10)
+
+ozzy.doginfo()
+skippy.doginfo()
+filou.doginfo()
+
+print(ozzy.age)
+ozzy.birthday()
+print(ozzy.age)
+
+ozzy.setBuddy(filou)
+print(ozzy.buddy.name)
+print(ozzy.buddy.age)
+
+skippy.buddy.doginfo()
